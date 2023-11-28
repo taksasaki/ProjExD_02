49bbadc92e90c59a770df68e6533e3f5eb86dcb5
佐々木貴大 <takasaki@sasakitakashidainoMacBook-Pro.local>
2023-11-28 15:18:09 +0900

    練習５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,16 +1,30 @@
 import sys
-import pygame as pg
 import random
+import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
 
-data = {
-    pg.K_UP:(0,-5),
-    pg.K_DOWN:(0,+5),
-    pg.K_LEFT:(-5, 0),
-    pg.K_RIGHT:(+5,0)    
+delta = {
+    pg.K_UP: (0, -5),  #キー：移動量/値
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0)
 }
+
+
+def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面買いを判定し、真理値タプルを返す関数
+    引数　rct：こうかとんor爆誕SurfaceのRect
+    戻り値：横方向、縦方向はみだし判定結果（画面内：Ttue/画面外：False）
+    """
+    yoko, tate = True, True
+    if rct.left <= 0 or WIDTH < rct.right:
+        yoko = False
+    if rct.top < 0 or HEIGHT < rct.bottom:
+        tate = False
+    return yoko, tate
 
 
 def main():
@@ -19,15 +33,16 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
-    bb_img = pg.Surface((20,20))  #練習１：透明のSurfaceを作る
-    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
-    bb_rct = bb_img.get_rect()
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 900,400
-    kk_rct.centery = (900,400)
-    bb_rct.centerx = random.randint(0,WIDTH)
-    bb_rct.centery = random.randint(0,HEIGHT)
-    
+    kk_rct.center = 900, 400
+    bb_img = pg.Surface((20, 20))  #練習1：透明のSurfaceを作る
+    bb_img.set_colorkey((0, 0, 0))
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  #練習1：中心に半径10の赤い円を書く
+    bb_rct = bb_img.get_rect()  
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)
+    vx, vy = +5, +5  #練習2：爆弾の速度
+
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -35,21 +50,33 @@ def main():
             if event.type == pg.QUIT: 
                 return
             
-        key_list = pg.key.get_pressed()
-        sum_mv = [0,0]
+            if kk_rct.colliderect(bb_rct):
+                print("Game Over")
+                return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
         for k, tpl in delta.items():
-            if key_list[k]:
+            if key_lst[k]:  #キーが押されたら
                 sum_mv[0] += tpl[0]
-                sum_mv[0] += tpl[1]
+                sum_mv[1] += tpl[1]
+        
+
 
         screen.blit(bg_img, [0, 0])
-        kk_rct.move_ip(sum_mv[0],sum_mv[1])
-        kk_rct
-        screen.blit(kk_img, [900, 400])
-        screen.blit(bb_img,bb_rct)
+        kk_rct.move_ip(sum_mv[0], sum_mv[1])
+        if check_bound(kk_rct) != (True, True):
+            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:  #横方向はみ出たら
+            vx *= -1
+        if not tate:  #縦方向はみ出たら
+            vy *= -1
+        screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
-        clock.tick(10)
         clock.tick(50)
 
 
